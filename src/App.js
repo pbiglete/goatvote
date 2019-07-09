8f5a7ee03ff83bf0a9c9664e9d9d9b3dcb504c66
Paul Jerrold Biglete <pbiglete@gmail.com>
2019-07-09 16:32:22 -0700

    API Call Test - Working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        players: [],
+        isLoaded: false,
+      }
+    }
+
+    componentDidMount() {
+      fetch('https://www.balldontlie.io/api/v1/players/1043')
+        .then(result => result.json())
+        .then(json => {
+            this.setState({
+              isLoaded: true,
+              players: json,
+            })
+        });
+    }
+
+    render() {
+
+      var { isLoaded, players } = this.state;
+
+      if(!isLoaded) {
+        return <div>Loading...</div>;
+      }
+      else {
+          return (
+            <div className="App">
+                {players.first_name} {players.last_name}
+            </div>
+        );
+      }
+    }
 }
 
 export default App;
